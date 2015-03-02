9ff59cb042182b974234f3a9ff66471f966d39bc
Alexandre Gois <adesouz@wal-mart.com>
2015-03-02 15:05:02 -0800

    PG DB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -9,6 +10,20 @@ app.get('/', function(request, response) {
 	//response.send('Hello World!');
 	response.send(cool());
 });
+
+app.get('/db', function (request, response) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query('SELECT * FROM data_table', function(err, result) {
+			done();
+			if (err) {
+			    console.error(err);
+			    response.send("Error " + err);
+			} else {
+			    response.send(result.rows);
+			}
+		    });
+	    });
+    });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
