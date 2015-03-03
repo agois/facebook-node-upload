81e0ea65b9b62087818fa8b7cdc79d9be76c05fa
Alexandre Gois <adesouz@wal-mart.com>
2015-03-02 16:20:21 -0800

    upload

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
 var pg = require('pg');
+var multer = require('multer');
+
+var UPLOAD_PATH = "./uploads/";
+
+// configure multer for upload management
+var fileUploadCompleted = false;
+var multerFiles = multer({ dest: UPLOAD_PATH,
+	rename: function (fieldname, filename) {
+		return filename;
+	},
+	onFileUploadStart: function (file) {
+		console.log("Started upload of: " + file.originalname);
+	},
+	onFileUploadComplete: function (file) {
+		console.log("Finished upload of: " + file.fieldname + " to: " + file.path);
+		fileUploadCompleted = true;
+	}
+});
+
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+
+// ENDPOINTS
 app.get('/', function(request, response) {
 	//response.send('Hello World!');
 	response.send(cool());
@@ -23,6 +44,14 @@ app.get('/db', function (request, response) {
 			}
     });
   });
+});
+
+// handle uploads
+app.post('/upload', multerFiles, function(req, res) {
+	if (fileUploadCompleted){
+		fileUploadCompleted = false;
+		res.end("Upload Ok!");
+	}
 });
 
 app.listen(app.get('port'), function() {
