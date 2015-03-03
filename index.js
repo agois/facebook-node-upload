fb1d53a551f75cef64902df88ae854a0f67b217c
Alexandre Gois <adesouz@wal-mart.com>
2015-03-03 10:16:23 -0800

    fix

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ var UPLOAD_PATH = "./uploads/";
 // configure multer for upload management
 var fileUploadCompleted = false;
 var multerFiles = multer({ dest: UPLOAD_PATH,
-	rename: function (fieldname, filename) {
-		return filename;
-	},
-	onFileUploadStart: function (file) {
-		console.log("Started upload of: " + file.originalname);
-	},
-	onFileUploadComplete: function (file) {
-		console.log("Finished upload of: " + file.fieldname + " to: " + file.path);
-		fileUploadCompleted = true;
-	}
+    rename: function (fieldname, filename) {
+        return filename;
+    },
+    onFileUploadStart: function (file) {
+        console.log("Started upload of: " + file.originalname);
+    },
+    onFileUploadComplete: function (file) {
+        console.log("Finished upload of: " + file.fieldname + " to: " + file.path);
+        fileUploadCompleted = true;
+    }
 });
 
 
@@ -28,30 +28,35 @@ app.use(express.static(__dirname + '/public'));
 
 // ENDPOINTS
 app.get('/', function(request, response) {
-	//response.send('Hello World!');
-	response.send(cool());
+    //response.send('Hello World!');
+    response.send(cool());
 });
 
 app.get('/db', function (request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM data_table', function(err, result) {
-			done();
-			if (err) {
-			    console.error(err);
-			    response.send("Error " + err);
-			} else {
-			    response.send(result.rows);
-			}
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM data_table', function(err, result) {
+            done();
+            if (err) {
+                console.error(err);
+                response.send("Error " + err);
+            } else {
+                response.send(result.rows);
+            }
     });
   });
 });
 
 // handle uploads
 app.post('/upload', multerFiles, function(req, res) {
-	if (fileUploadCompleted){
-		fileUploadCompleted = false;
-		res.end("Upload Ok!");
-	}
+    if (fileUploadCompleted){
+        fileUploadCompleted = false;
+        res.end("Upload Ok!");
+    }
+});
+
+// handle uploads
+app.post('/uploadFacebook', function(req, res) {
+    imageURL = req.body.imageURL;
 });
 
 app.listen(app.get('port'), function() {
